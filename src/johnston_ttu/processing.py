75c88eeac18dea4ffde02d96ee9dd25bf8b7842e
Derek Johnston <74471900+derek-johnston@users.noreply.github.com>
2023-10-03 17:10:55 -0500

    Locking changes for the day.

--- src/johnston_ttu/processing.py
+++ src/johnston_ttu/processing.py
@@ -1,3 +1,6 @@
+from os import listdir, remove
+import numpy as np
+import pandas as pd
 """
 processing.py
 Developed By: Derek Johnston @ Texas Tech University
@@ -6,7 +9,7 @@ Special-purpose functions for data cleaning and pre-processing.
 """
 def remove_ena_labels(filename):
 	"""
-	They Keysight ENA creates two extra lines of metadata at the 
+	The Keysight ENA creates two extra lines of metadata at the 
 	top of a measurement .CSV file. These should be removed so
 	that datafiles can be imported into Pandas.
 	
@@ -16,7 +19,38 @@ def remove_ena_labels(filename):
 	line_buffer = [] # Pull each line out of the file, and write back all but the last two.
 	with open(f"{filename}.csv", "r") as fp:
 		line_buffer = fp.readlines()
-	with open(f"{filename}_p.csv", "w") as fp:
+	with open(f"{filename}_t.csv", "w") as fp:
 		for number, line in enumerate(line_buffer):
 			if number >= 2:
 				fp.write(line)
+
+def process_ena_data(directory):
+	"""
+	For a given directory containing raw ENA data, process the data and generate
+	new columns for the complex value, magnitude, and phase information.
+
+	Keyword Arguments:
+	directory -- The folder containing the datafiles.
+	"""
+	# Get a list of all the datafiles in the given directory.
+	directories = listdir(directory)
+	# Iterate through all the files in the directory.
+	for file in directories:
+		# Remove the .csv tag from the end of the file.
+		file = file.replace(".csv", "")
+		# Remove the ENA metadata lines.
+		remove_ena_labels(f"{directory}\{file}")
+		# Read-in the temporary file as a pandas dataframe.
+		df = pd.read_csv(f"{directory}\{file}_t.csv")
+		df.rename(columns={" Formatted Data": "Real", " Formatted Data.1": "Imag"}, inplace=True)
+		# Get the complex number for each frequency component.
+		df["Complex"] = [complex(df["Real"][idx], df["Imag"][idx]) for idx in df.index]
+		# Get the magnitude and phase for each row of the dataframe.
+		df["Magnitude"] = [np.abs(df["Complex"][idx]) for idx in df.index]
+		df["Phase"] 		= [np.angle(df["Complex"][idx]) for idx in df.index]
+		print(df.head())
+		# Remove the temporary data file.
+		remove(f"{directory}\{file}_t.csv")
+	
+if __name__ == "__main__":
+	process_ena_data("data")
